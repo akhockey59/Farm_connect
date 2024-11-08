be536a7716ad75b0eb6e1e22b5ddfa08bcf5dd34
Aakash <143587104+akhockey59@users.noreply.github.com>
2024-11-09 02:30:57 +0530

    Update index.js

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -12,7 +12,7 @@ export const registerUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5001/api/auth/register",
+      "https://farm-backend-ouo1.onrender.com/api/auth/register",
       formData,
       {
         withCredentials: true,
@@ -28,7 +28,7 @@ export const loginUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5001/api/auth/login",
+      "https://farm-backend-ouo1.onrender.com/api/auth/login",
       formData,
       {
         withCredentials: true,
@@ -44,7 +44,7 @@ export const logoutUser = createAsyncThunk(
 
   async () => {
     const response = await axios.post(
-      "http://localhost:5001/api/auth/logout",
+      "https://farm-backend-ouo1.onrender.com/auth/logout",
       {},
       {
         withCredentials: true,
@@ -60,7 +60,7 @@ export const checkAuth = createAsyncThunk(
 
   async () => {
     const response = await axios.get(
-      "http://localhost:5001/api/auth/check-auth",
+      "https://farm-backend-ouo1.onrender.com/api/auth/check-auth",
       {
         withCredentials: true,
         headers: {
