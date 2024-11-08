d5ac16ba3bdcde91c00d9eeb46f7909931121b89
Aakash <143587104+akhockey59@users.noreply.github.com>
2024-11-09 02:36:26 +0530

    Update index.js

--- client/src/store/admin/order-slice/index.js
+++ client/src/store/admin/order-slice/index.js
@@ -10,7 +10,7 @@ export const getAllOrdersForAdmin = createAsyncThunk(
   "/order/getAllOrdersForAdmin",
   async () => {
     const response = await axios.get(
-      `http://localhost:5001/api/admin/orders/get`
+      `https://farm-backend-ouo1.onrender.com/api/admin/orders/get`
     );
 
     return response.data;
