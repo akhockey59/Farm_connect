51b2ddfc41d60f88bf80023021fd290da29aca9b
Aakash <143587104+akhockey59@users.noreply.github.com>
2024-11-09 02:27:54 +0530

    Update index.js

--- client/src/store/admin/order-slice/index.js
+++ client/src/store/admin/order-slice/index.js
@@ -21,7 +21,7 @@ export const getOrderDetailsForAdmin = createAsyncThunk(
   "/order/getOrderDetailsForAdmin",
   async (id) => {
     const response = await axios.get(
-      `http://localhost:5001/api/admin/orders/details/${id}`
+      `https://farm-backend-ouo1.onrender.com/api/admin/orders/details/${id}`
     );
 
     return response.data;
@@ -32,7 +32,7 @@ export const updateOrderStatus = createAsyncThunk(
   "/order/updateOrderStatus",
   async ({ id, orderStatus }) => {
     const response = await axios.put(
-      `http://localhost:5001/api/admin/orders/update/${id}`,
+      `https://farm-backend-ouo1.onrender.com/api/admin/orders/update/${id}`,
       {
         orderStatus,
       }
